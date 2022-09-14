f45b5c3c75365606ed5a654a4c5e386038bdb424
Jeremy Holtzman <jeremyholtzman@gmail.com>
2022-09-14 16:20:18 -0400

    setup tests

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -16,10 +16,13 @@ final as (
     select 
         customer.customer_key,
         customer.customer_name,
+        {# nation.nation_key as nation_key, #}
+        nation.nation_name as nation,
+        {# region.region_key as region_key, #}
+        region.region_name as region,
         customer.customer_phone,
-        customer.customer_acct_balance,
-        nation.nation_key as nation_key,
-        region.region_key as region_key
+        customer.customer_acct_balance
+        -- new column
     from
         customer
         inner join nation
--- models/staging/sample_data/stg_nations.sql
+++ models/staging/sample_data/stg_nations.sql
@@ -10,6 +10,7 @@ transformed as (
 
   select 
     n_nationkey as nation_key,
+    n_name as nation_name,
     n_regionkey as region_key
     
 
